7b90da4eb136453f491e4e0fdff8339d10633f30
swChoi <chsw030818@naver.com>
2025-10-01 21:59:04 +0900

    로그인 화면 구성 및 User model, UserCreationForm 생성

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
-
+from django.contrib.auth.forms import AuthenticationForm
 # Create your views here.
 def login(request):
-    pass
+    form = AuthenticationForm()
+    context = {
+        'form': form,
+    }
+    return render(request, "accounts/login.html", context)
